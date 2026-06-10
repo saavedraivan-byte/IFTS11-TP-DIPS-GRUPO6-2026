e7aaf67e5bf3eba3c05bab6b81a808d9446146aa
fabrioliverazxz <fabriolivera0@gmail.com>
2026-06-09 22:36:09 -0300

    Update api.cy.js

--- cypress/e2e/api.cy.js
+++ cypress/e2e/api.cy.js
@@ -43,3 +43,34 @@ describe('Casos de prueba de APIs', () => {
 
 
 })
+
+describe('Casos de prueba de APIs - Módulo Libros | Fabrizio Olivera', () => {
+
+    it.only('API | Obtener detalle de un libro existente por ID | Fabrizio Olivera', () => {
+        
+        cy.loginRequest(user.username, user.password).then((token) => {
+            const bookId = 2;
+            
+            cy.getBookByIdAPI(bookId, token).then((response) => {
+                expect(response.status).to.eq(200);
+                expect(response.body).to.have.property('bookId', bookId);
+                expect(response.body).to.have.property('title');
+                expect(response.body).to.have.property('author');
+            });
+        });
+
+    });
+    
+    it.only('API | Validar código de error al consultar un libro inexistente | Fabrizio Olivera', () => {
+        
+        cy.loginRequest(user.username, user.password).then((token) => {
+            const bookIdInexistente = 1;
+
+            cy.getBookByIdAPI(bookIdInexistente, token).then((response) => {
+                expect(response.status).to.eq(404);
+            });
+        });
+
+    });
+
+});
